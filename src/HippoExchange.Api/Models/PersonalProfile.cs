12abb43144640bb41c8790cd8ef0f283c92ac8c9
DaltonSloan <Dwsloan42@Gmail.com>
2025-09-19 23:14:55 +0000

    fix: Add BsonIgnoreIfDefault to prevent null _id insertion

--- src/HippoExchange.Api/Models/PersonalProfile.cs
+++ src/HippoExchange.Api/Models/PersonalProfile.cs
@@ -8,6 +8,7 @@ public class PersonalProfile
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
+    [BsonIgnoreIfDefault]
     public string? Id { get; set; }
     public string UserId { get; set; } = default!;
     public string FullName { get; set; } = "";
