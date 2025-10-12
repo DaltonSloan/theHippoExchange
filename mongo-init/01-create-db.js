7f7d55481423bbe48f1a45784e27ac0a3841b5db
TaylorTurnerIT <tjturner44@tntech.edu>
2025-10-12 04:28:31 +0000

    test

--- mongo-init/01-create-db.js
+++ mongo-init/01-create-db.js
@@ -1,10 +1,14 @@
-// Creates an application user (optional if using only root in dev)
+// Creates the database and collections for HippoExchange
 db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE || "HippoExchange");
 
-// Profiles Collection
-db.createCollection("profiles");
-db.profiles.createIndex({ UserId: 1 }, { unique: true });
+// Users Collection
+db.createCollection("users");
+db.users.createIndex({ ClerkId: 1 }, { unique: true });
 
 // Assets Collection
 db.createCollection("assets");
 db.assets.createIndex({ OwnerUserId: 1 });
+
+// Maintenance Collection
+db.createCollection("maintenance");
+db.maintenance.createIndex({ AssetId: 1 });
