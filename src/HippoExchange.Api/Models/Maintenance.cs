9f26282ddfdde803043d30e860a7519ba4931164
DaltonSloan <Dwsloan42@Gmail.com>
2025-10-15 06:43:53 +0000

    IsComplete needs to be nullable.

--- src/HippoExchange.Api/Models/Maintenance.cs
+++ src/HippoExchange.Api/Models/Maintenance.cs
@@ -150,7 +150,7 @@ namespace HippoExchange.Api.Models
         
         public bool PreserveFromPrior { get; set; }
         
-        public bool IsCompleted { get; set; } = false;
+        public bool IsCompleted { get; set; }
 
         [Required(ErrorMessage = "At least one required tool must be specified.")]
         public List<string> RequiredTools { get; set; } = new List<string>();
